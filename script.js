6a7cf702114cc68ba6f20e4d4651be7c947e4c54
annadehtiarova <60813021+annadehtiarova@users.noreply.github.com>
2024-10-09 20:58:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('submitBtn').addEventListener('click', function(event) {
-        const emailInput = document.getElementById('email');
-        const errorMessage = document.querySelector('.error-message');
-
-        
-        // Check if the input is empty or invalid
-        if (!emailInput.value || !emailInput.checkValidity()) {
-            event.preventDefault(); // Prevent form submission
-            emailInput.classList.add('error'); // Add error class
-            errorMessage.style.display = 'block'; // Show error message
-        }
-
-        // Listener for input event to reset error state
-        emailInput.addEventListener('input', function() {
-        // Remove error state when user starts typing
-        emailInput.classList.remove('error');
-        errorMessage.style.display = 'none';
-    });
-    });
-});*/
 
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('submitBtn').addEventListener('click', function(event) {
