27b4c5b51ea98e27924c47260a1182671c16a09c
annadehtiarova <60813021+annadehtiarova@users.noreply.github.com>
2024-10-09 22:24:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('submitBtn').addEventListener('click', function(event) {
         const emailInput = document.getElementById('email');
         const errorMessage = document.querySelector('.error-message');
+        const clearBtn = document.getElementById('clearBtn');
+        const submitBtn = document.getElementById('submitBtn');
 
         function isValidEmail(email) {
-        // Regular expression to check for '@' and '.' in the email
-            const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
-            return regex.test(email);
-        }
-        
+            // Regular expression to check for '@' and '.' in the email
+                const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+                return regex.test(email);
+            }
+
+        // Listener for the submit button
+        submitBtn.addEventListener('click', function(event) {
         // Check if email is correct or input exists, if not provide an error
         if (!isValidEmail(emailInput.value) || !emailInput.value) {
             event.preventDefault(); // Prevent form submission
@@ -22,6 +25,35 @@ document.addEventListener('DOMContentLoaded', function() {
         // Remove error state when user starts typing
         emailInput.classList.remove('error');
         errorMessage.style.display = 'none';
+        
+        // Show the clear button when the user types something
+        if (emailInput.value.length > 0) {
+            clearBtn.style.display = 'block';
+        } else {
+            clearBtn.style.display = 'none';
+        }
+        });
+
     });
-    });
+
+
+        // Listener
+        emailInput.addEventListener('input', function() {
+        
+        // Show the clear button when the user types something
+        if (emailInput.value.length > 0) {
+            clearBtn.style.display = 'block';
+        } else {
+            clearBtn.style.display = 'none';
+        }
+        });
+
+
+        // Listener for the claer button
+        clearBtn.addEventListener('click', function() {
+        emailInput.value = ''; // Clear the input field
+        emailInput.classList.remove('error'); // Remove the error state if present
+        errorMessage.style.display = 'none'; // Hide the error message
+        clearBtn.style.display = 'none'; // Hide the clear button again
+        });
 });
